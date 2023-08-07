383a224667aad8e5a9f648ee66c04a4922f7bfb0
paperbox <lego37yoon@outlook.com>
2023-08-07 23:23:06 +0900

    (feat) apply SUITE font to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,11 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import localFont from 'next/font/local'
 
-const inter = Inter({ subsets: ['latin'] })
+const suite = localFont({
+  src: '../../public/font/SUITE-Variable.woff2',
+  display: 'swap'
+});
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -16,7 +19,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={suite.className}>{children}</body>
     </html>
   )
 }
