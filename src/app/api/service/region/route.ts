532df2ff8b8dd6111e9a3c5a718efd5f289eb37a
paperbox <lego37yoon@outlook.com>
2023-08-23 22:59:51 +0900

    (feat) add get service region API

---
+++ src/app/api/service/region/route.ts
@@ -0,0 +1,30 @@
+import { NextResponse } from "next/server";
+import data from "./region.json";
+
+type StateType = {
+    code: string,
+    name: string
+}
+
+type RegionType = {
+    state: StateType[],
+    city: {[key: string]: StateType[]}
+}
+
+export async function GET(request: Request) {
+    const region: RegionType = data;
+    const { searchParams } = new URL(request.url);
+    const type: string | null = searchParams.get('type');
+    const state: string | null = searchParams.get('state');
+
+    switch(type) {
+        case "state":
+            return NextResponse.json(region.state);
+        case "city":
+            if (state !== null)
+                return NextResponse.json(region.city[state]);
+            break;
+        default:
+            return NextResponse.json({error: "잘못된 지역 분류입니다."}, {status: 400});
+    }
+}
