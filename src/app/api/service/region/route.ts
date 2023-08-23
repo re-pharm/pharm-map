59a57aaa7b830f880c031ee60714f5bacf42f974
paperbox <lego37yoon@outlook.com>
2023-08-24 00:18:24 +0900

    (feat) return when type=geo

--- src/app/api/service/region/route.ts
+++ src/app/api/service/region/route.ts
@@ -16,6 +16,7 @@ export async function GET(request: Request) {
     const { searchParams } = new URL(request.url);
     const type: string | null = searchParams.get('type');
     const state: string | null = searchParams.get('state');
+    const city: string | null = searchParams.get('city');
 
     switch(type) {
         case "state":
@@ -25,6 +26,23 @@ export async function GET(request: Request) {
                 return NextResponse.json(region.city[state]);
             else
                 return NextResponse.json({error: "없는 지역입니다."}, {status: 404});
+        case "geo":
+            const stateResult = region.state.find((province) => province.name === state);
+
+            if (stateResult !== undefined) {
+                const cityResult = region.city[stateResult.name].find((town) => town.name === city);
+                if (cityResult !== undefined) {
+                    return NextResponse.json({
+                        state: stateResult,
+                        city: cityResult
+                    });
+                } else {
+                    return NextResponse.json({error: "찾고 있는 지역이 없습니다."}, {status: 400});
+                }
+            } else {
+                return NextResponse.json({error: "찾고 있는 지역이 없습니다."}, {status: 400});
+            }
+            break;
         default:
             return NextResponse.json({error: "잘못된 지역 분류입니다."}, {status: 400});
     }
