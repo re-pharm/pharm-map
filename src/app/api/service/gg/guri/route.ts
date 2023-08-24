bda31355d015612ae0b7580cff143d4068c48f53
paperbox <lego37yoon@outlook.com>
2023-08-25 01:07:33 +0900

    (cont) add Guri city data (exclude pharm)

---
+++ src/app/api/service/gg/guri/route.ts
@@ -0,0 +1,23 @@
+import { NextResponse } from "next/server";
+
+export async function GET(request: Request) {
+    const { searchParams } = new URL(request.url);
+    const page: string | null = searchParams.get('page');
+    
+    const guriBox = await fetch(`https://api.odcloud.kr/api/15074686/v1/uddi:969c010f-aa4c-4e89-aedf-f933371d1c44?serviceKey=${process.env.DATA_GO_KR_REST_KEY}`);
+    const guriBoxData = await guriBox.json();
+
+    if (guriBox.ok) {
+        return NextResponse.json({
+            data: guriBoxData.data,
+            structure: {
+                name: "설치 장소",
+                location: "위치(도로명)",
+                tel: "전화번호"
+            },
+            date: "20230329"
+        })
+    } else {
+        return NextResponse.json({ error: guriBoxData.msg}, { status: guriBox.status });
+    }
+}
