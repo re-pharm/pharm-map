a9f5b9bcc624aa4f517e39e1255d646bc56c35c5
paperbox <lego37yoon@outlook.com>
2023-08-25 23:58:34 +0900

    (feat) add pharmacy to Guri city

--- src/app/api/service/gg/guri/route.ts
+++ src/app/api/service/gg/guri/route.ts
@@ -1,24 +1,52 @@
 import { NextResponse } from "next/server";
 
+type Data = {
+    [index: string]: string,
+    name: string,
+    location: string,
+    tel: string,
+    lat: string,
+    lng: string
+}
+
 export async function GET(request: Request) {
     const { searchParams } = new URL(request.url);
     const page: string | null = searchParams.get('page');
     
-    const guriBox = await fetch(`https://api.odcloud.kr/api/15074686/v1/uddi:969c010f-aa4c-4e89-aedf-f933371d1c44?serviceKey=${process.env.DATA_GO_KR_REST_KEY}`);
-    const guriBoxData = await guriBox.json();
+    try {
+        //0: 폐의약품 수거함 위치, 1: 폐의약품 수거 약국 위치
+        const guriList = await Promise.all([
+            fetch(`https://api.odcloud.kr/api/15074686/v1/uddi:969c010f-aa4c-4e89-aedf-f933371d1c44?serviceKey=${process.env.DATA_GO_KR_REST_KEY}`).then((res) => res.json()),
+            fetch(`https://api.odcloud.kr/api/15074687/v1/uddi:88f1e037-d8b2-46a8-8aec-ed4316180345?perPage=90&serviceKey=${process.env.DATA_GO_KR_REST_KEY}`).then((res) => res.json())
+        ]);
+        const data: Data[] = [];
+        
+        guriList[0].data.forEach((place: Data) => {
+            data.push({
+                name: place["설치 장소"],
+                location: place["위치(도로명)"],
+                tel: place["전화번호"],
+                lat: place["위도"],
+                lng: place["경도"]
+            });
+        });
 
-    if (guriBox.ok) {
-        return NextResponse.json({
-            data: guriBoxData.data,
-            structure: {
-                name: "설치 장소",
-                location: "위치(도로명)",
-                tel: "전화번호"
-            },
-            date: "20230329"
+        guriList[1].data.forEach((place: Data) => {
+            data.push({
+                name: place["약국명칭"],
+                location: place["약국소재지(도로명)"],
+                tel: place["전화번호"],
+                lat: place["위도"],
+                lng: place["경도"]
+            })
         })
-    } else {
-        return NextResponse.json({ error: guriBoxData.msg}, { status: guriBox.status });
+
+        return NextResponse.json({
+            data: data,
+            date: "20230607"
+        });
+    } catch(e) {
+        return NextResponse.json({ error: e}, { status: 500 });
     }
 }
 
