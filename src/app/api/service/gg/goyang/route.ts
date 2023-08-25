dd20ae5a51a99621b6e263fc5e82c1ea921b6ce0
paperbox <lego37yoon@outlook.com>
2023-08-25 22:03:06 +0900

    (feat) support location of address in goyang

--- src/app/api/service/gg/goyang/route.ts
+++ src/app/api/service/gg/goyang/route.ts
@@ -1,22 +1,8 @@
 import { NextResponse } from "next/server";
+import goyang from "./goyang.json";
 
 export async function GET() {
-    const goyangPharm = await fetch(`https://api.odcloud.kr/api/15077990/v1/uddi:22309806-43d2-49c4-a2ed-c316bb12723d?serviceKey=${process.env.DATA_GO_KR_REST_KEY}`);
-    const goyangData = await goyangPharm.json();
-
-    if (goyangPharm.ok) {
-        return NextResponse.json({
-            data: goyangData.data,
-            structure: {
-                name: "영업소명",
-                location: "영업소소재지(도로명)",
-                tel: "영업소전화번호"
-            },
-            date: "20230105"
-        })
-    } else {
-        return NextResponse.json({ error: goyangData.msg}, { status: goyangPharm.status });
-    }
+    return NextResponse.json(goyang);
 }
 
 export const runtime = 'edge';
