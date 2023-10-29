c71078ac82034018d4f6d549e1553efb776ac3b2
paperbox <lego37yoon@outlook.com>
2023-10-30 00:35:49 +0900

    (feat) click marker to get box info

--- src/app/components/kakaomap/Kmap.tsx
+++ src/app/components/kakaomap/Kmap.tsx
@@ -3,6 +3,7 @@
 import Script from 'next/script';
 import { useCallback, useEffect, useState } from 'react';
 import "./kmap.css";
+import { useRouter } from 'next/router';
 
 type Props = {
   latLng: {
@@ -12,6 +13,8 @@ type Props = {
 
   data?: {
     name: string,
+    state: string,
+    city: string,
     lat: string,
     lng: string
   }[]
@@ -19,7 +22,9 @@ type Props = {
 
 export default function Kmap(prop: Props) {
   const [map, generateMap] = useState<any>();
+  const router = useRouter();
 
+  //맵 초기화
   const initMap = useCallback(() => {
     const container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
     const options = { //지도를 생성할 때 필요한 기본 옵션
@@ -29,23 +34,32 @@ export default function Kmap(prop: Props) {
     generateMap(new (window as any).kakao.maps.Map(container, options)); //지도 생성 및 객체 리턴
   }, [prop]);
 
+  // 맵 중심 지점 설정
   useEffect(() => {
     if (map) {
       map.setCenter(new (window as any).kakao.maps.LatLng(prop.latLng.lat, prop.latLng.lng));
     }
   }, [prop.latLng, map])
 
+  // 맵에 수거함 위치 설정
   useEffect(() => {
+    function loadPharmBoxInfo(title: string, state: string, city: string) {
+      router.push(`${state}/${city}/${title}`);
+    }
+
     if (map && prop.data) {
       prop.data.forEach((place) => {
         const marker = new (window as any).kakao.maps.Marker({
           map: map,
           position: new (window as any).kakao.maps.LatLng(place.lat, place.lng),
           title: place.name
-        })
+        });
+
+        (window as any).kakao.maps.event.addListener(marker, "click", 
+          loadPharmBoxInfo(marker.title, place.state, place.city));
       })
     }
-  }, [prop.data, map])
+  }, [prop.data, map, router]);
 
   return (
     <>
