777264fa83b34017af5c2ef9a4744dd91367938e
paperbox <lego37yoon@outlook.com>
2023-10-31 15:57:19 +0900

    (fix) cannot read location data when url changed
    - make variable names more clear
    - use Context API, Session Storage, State to manage current data
    - add button to delete current valid data

--- src/app/components/locations/CurrentLocationButton.tsx
+++ src/app/components/locations/CurrentLocationButton.tsx
@@ -2,65 +2,38 @@
 import { useRouter } from "next/navigation";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEraser, faLocationCrosshairs } from '@fortawesome/free-solid-svg-icons'
-import { useEffect, useState } from "react";
-
-export type CurrentLoc = {
-    lat: string,
-    lng: string
-}
-
-//지역 값 검증
-export async function validateRegionValue(state: string, city?: string) {
-    if (city) {
-        const validateResult = await fetch(`/api/service/supported_region?type=geo&state=${state}&city=${city}`);
-        const validateResultJson = await validateResult.json();
-
-        if (validateResult.ok) {
-            return {
-                status: true,
-                state: validateResultJson.state,
-                city: validateResultJson.city
-            }
-        }
-        
-        return {
-            status: false,
-            message: validateResultJson.error
-        }
-    } else {
-        const validateResult = await fetch(`/api/service/supported_region?type=city&state=${state}`);
-        
-        if (validateResult.ok) {
-            return {
-                status: true
-            }
-        }
-
-        return {
-            status: false
-        }
-    }    
-}
+import { RealtimeLocationData } from "@/app/types/locationdata";
+import { useContext, useEffect, useState } from "react";
 
 export default function CurrentLocationButton() {
     const router = useRouter(); //페이지 이동
-    const [currentLoc, setCurrentLocation] = useState<CurrentLoc|undefined>(undefined);
+    const pageCurrentLoc = useContext(RealtimeLocationData);
+    const [isGeolocationEnabled, enableGeolocation] = useState<Boolean>(false);
 
     //현위치 버튼 눌렀을 때의 동작
     function getGeolocation(e: React.MouseEvent<HTMLButtonElement>) {
         e.preventDefault();
         if ("geolocation" in navigator) {
             navigator.geolocation.getCurrentPosition(async ({ coords }) => {
-                const currentLoc = await fetch(`/api/geo/region?lat=${coords.latitude}&lng=${coords.longitude}`);
-                const currentLocResult = await currentLoc.json();
-                const currentRegionResult = await validateRegionValue(currentLocResult.state, currentLocResult.city);
+                sessionStorage.setItem("lat", coords.latitude.toString());
+                sessionStorage.setItem("lng", coords.longitude.toString());
+                const getCurrentRegion = await fetch(`/api/geo/region?lat=${coords.latitude}&lng=${coords.longitude}`);
+                const currentRegion = await getCurrentRegion.json();
+                const validateLocation =
+                    await fetch(`/api/service/supported_region?type=geo&state=${currentRegion.state}&city=${currentRegion.city}`);
+                const validateResult = await validateLocation.json();
 
-                if (currentRegionResult.status) {
-                    sessionStorage.setItem("lat", coords.latitude.toString());
-                    sessionStorage.setItem("lng", coords.longitude.toString());
-                    router.push(`/${currentRegionResult.state.code}/${currentRegionResult.city.code}`);
+                if (validateLocation.ok) {
+                    router.push(`/${validateResult.state.code}/${validateResult.city.code}`);
+                    if (pageCurrentLoc) {
+                        pageCurrentLoc.set({
+                            lat: coords.latitude.toString(),
+                            lng: coords.longitude.toString()
+                        });
+                    }
+                    enableGeolocation(true);
                 } else {
-                    alert(`${currentRegionResult.message} 서울특별시인 경우, 스마트 서울맵을 이용하세요.`);
+                    alert(`${validateResult.error} 서울특별시인 경우, 스마트 서울맵을 이용하세요.`);
                 }
             }, (error) => {
                 console.log(error);
@@ -73,41 +46,34 @@ export default function CurrentLocationButton() {
     }
 
     useEffect(() => {
-        const lat = sessionStorage.getItem("lat");
-        const lng = sessionStorage.getItem("lng");
-        
-        if (lat && lng) {
-            setCurrentLocation({
-                lat: lat,
-                lng: lng
-            });
+        if (pageCurrentLoc && pageCurrentLoc.value) {
+            enableGeolocation(true)
         } else {
-            setCurrentLocation(undefined);
+            enableGeolocation(false);
         }
-    }, [])
+    }, [pageCurrentLoc])
 
-    function removeCurrentLocation() {
+    function removeLocationInfo(e: React.MouseEvent<HTMLButtonElement>) {
         sessionStorage.removeItem("lat");
         sessionStorage.removeItem("lng");
-        router.refresh();
     }
 
     return (
-        <div id="locations" className="flex gap-2">
-            <button 
-            className="hover:bg-slate-100 hover:dark:bg-slate-600 dark:bg-slate-800 rounded-xl p-2"
-            onClick={(e) => {getGeolocation(e)}}>
+        <div className="flex gap-2">
+            <button
+                className="hover:bg-slate-100 hover:dark:bg-slate-600 dark:bg-slate-800 rounded-xl p-2"
+                onClick={(e) => {getGeolocation(e)}}>
                 <FontAwesomeIcon icon={faLocationCrosshairs} className="pe-1" />
                 현위치
             </button>
-            {currentLoc ? (
+            {isGeolocationEnabled ? (
                 <button
-                className="hover:bg-slate-100 hover:dark:bg-slate-600 dark:bg-slate-800 rounded-xl p-2"
-                onClick={(e) => {removeCurrentLocation()}}>
-                    <FontAwesomeIcon icon={faEraser} className="pe-1" />
+                    className="hover:bg-slate-100 hover:dark:bg-slate-600 dark:bg-slate-800 rounded-xl p-2"
+                    onClick={(e) => {removeLocationInfo(e)}}>
+                    <FontAwesomeIcon icon={faEraser}className="pe-1" />
                     위치정보 지우기
                 </button>
-            ) : ""}
-        </div>    
+            ):""}
+        </div>
     )
 }
