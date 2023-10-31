901e3115356ce09b041c00fd0439fe2e25057399
paperbox <lego37yoon@outlook.com>
2023-10-31 15:57:55 +0900

    (feat) make common part as component

---
+++ src/app/components/layouts/Header.tsx
@@ -0,0 +1,24 @@
+import { faPaperPlane } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import CurrentLocationButton from "../locations/CurrentLocationButton";
+
+export default function Header() {
+    return (
+        <header>
+            <h1 className="text-2xl">
+                <a href="/" className="no-underline">
+                    <span className="blockText text-sm">우리동네</span>
+                    폐의약품 수거지도 💊
+                </a>
+            </h1>
+            <nav id="mainFunctions" className="flex gap-2">
+                <CurrentLocationButton />
+                <a href="https://forms.gle/EST5vaZBFGy8DHGE8" target="_blank"
+                    className="hover:bg-slate-100 hover:dark:bg-slate-600 dark:bg-slate-800 rounded-xl p-2 no-underline">
+                    <FontAwesomeIcon icon={faPaperPlane} className="pe-1" />
+                    문의/제보
+                </a>                
+            </nav>
+        </header>
+    );
+}
