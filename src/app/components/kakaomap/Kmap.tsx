61ad7978f37dc526a2cbdd05ef540da1e60c26a1
paperbox <lego37yoon@outlook.com>
2023-10-31 15:30:26 +0900

    (feat) add info window for hovering mouse
    - (feat) share data types from page
    - (temp)(fix) map not loaded after changing URL
    - (feat) add click event to show information
    - (fix) use ref to prevent ID duplicate

--- src/app/components/kakaomap/Kmap.tsx
+++ src/app/components/kakaomap/Kmap.tsx
@@ -1,9 +1,10 @@
 'use client'
 
 import Script from 'next/script';
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import "./kmap.css";
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
+import type { Data } from '@/app/(result)/[state]/[city]/page';
 
 type Props = {
   latLng: {
@@ -11,63 +12,89 @@ type Props = {
     lng: Number
   },
 
-  data?: {
-    name: string,
-    state: string,
-    city: string,
-    lat: string,
-    lng: string
-  }[]
+  data?: Data[],
+  stateCode?: string,
+  cityCode?: string,
 }
 
 export default function Kmap(prop: Props) {
-  const [map, generateMap] = useState<any>();
   const router = useRouter();
+  const mapContainer = useRef(null);
+  const [map, generateMap] = useState<any>(undefined);
 
   //맵 초기화
-  const initMap = useCallback(() => {
-    const container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
-    const options = { //지도를 생성할 때 필요한 기본 옵션
-      center: new (window as any).kakao.maps.LatLng(prop.latLng.lat, prop.latLng.lng), //지도의 중심좌표.
-      level: 3 //지도의 레벨(확대, 축소 정도)
-    };
-    generateMap(new (window as any).kakao.maps.Map(container, options)); //지도 생성 및 객체 리턴
-  }, [prop]);
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src =
+      `https://dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_MAP_KEY}&autoload=false`;
+    document.head.appendChild(script);
 
-  // 맵 중심 지점 설정
+    script.onload = () => {
+      (window as any).kakao.maps.load(() => {
+        //지도 생성 및 객체 리턴
+        const container = mapContainer.current; //지도를 담을 영역의 DOM 레퍼런스
+        const options = { //지도를 생성할 때 필요한 기본 옵션
+          center: new (window as any).kakao.maps.LatLng(
+            prop.latLng.lat, 
+            prop.latLng.lng
+          ), //지도의 중심좌표.
+          level: 3 //지도의 레벨(확대, 축소 정도)
+        };
+        generateMap(new (window as any).kakao.maps.Map(container, options));
+      });
+    }
+  }, [prop.latLng, mapContainer]);
+
   useEffect(() => {
     if (map) {
-      map.setCenter(new (window as any).kakao.maps.LatLng(prop.latLng.lat, prop.latLng.lng));
+      map.setCenter(new (window as any).kakao.maps.LatLng(
+        prop.latLng.lat, prop.latLng.lng
+      ));
     }
-  }, [prop.latLng, map])
+  }, [prop.latLng, map]);
 
   // 맵에 수거함 위치 설정
   useEffect(() => {
     function loadPharmBoxInfo(title: string, state: string, city: string) {
-      router.push(`${state}/${city}/${title}`);
+      router.push(`/${state}/${city}/${title}`);
     }
 
-    if (map && prop.data) {
+    if (map && prop.data && prop.data.length > 0 && prop.stateCode && prop.cityCode) {
+      const state = prop.stateCode;
+      const city = prop.cityCode;
+
       prop.data.forEach((place) => {
         const marker = new (window as any).kakao.maps.Marker({
           map: map,
-          position: new (window as any).kakao.maps.LatLng(place.lat, place.lng),
-          title: place.name
+          position: new (window as any).kakao.maps.LatLng(place.lat, place.lng)
         });
 
-        (window as any).kakao.maps.event.addListener(marker, "click", 
-          loadPharmBoxInfo(marker.title, place.state, place.city));
-      })
+        const infoWindow = new (window as any).kakao.maps.InfoWindow({
+          content: `
+            <div class="w-full me-8">
+              <p class="text-center px-8">${place.name}</p>
+            </div>
+          `
+        });
+
+        (window as any).kakao.maps.event.addListener(marker, "click", function() {
+          loadPharmBoxInfo(place.name, state, city);
+        });
+
+        (window as any).kakao.maps.event.addListener(marker, "mouseover", function() {
+          infoWindow.open(map, marker);
+        });
+
+        (window as any).kakao.maps.event.addListener(marker, "mouseout", function() {
+          infoWindow.close();
+        });
+      });
     }
-  }, [prop.data, map, router]);
+  }, [prop.data, map, router, prop.stateCode, prop.cityCode]);
 
   return (
     <>
-      <Script
-          src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_MAP_KEY}&libraries=services,clusterer&autoload=false`}
-          onLoad={() => (window as any).kakao.maps.load(initMap)}
-      />
-      <section id="map" className="rounded-xl w-full hidden md:me-8 md:mt-8 md:block">
+      <section ref={mapContainer} className="rounded-xl w-full hidden md:me-8 md:mt-8 md:block">
       </section>
     </>
   )
