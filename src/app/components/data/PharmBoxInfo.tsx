df18b0704df3a45756f06fedb0d20b7ffd5078cb
paperbox <lego37yoon@outlook.com>
2023-11-01 20:22:25 +0900

    (feat) convert PharmBoxInfo into server component
    - fixes blank area issue in KakaoMap
    - faster loading, better SEO
    - less Cumulative Layout Shift(CLS)

--- src/app/components/data/PharmBoxInfo.tsx
+++ src/app/components/data/PharmBoxInfo.tsx
@@ -1,9 +1,6 @@
-"use client"
-import { faRoute, faXmark, faCapsules, faBuildingColumns, faPhone, faLocationDot, faClock, faCheckDouble, faInfoCircle } from "@fortawesome/free-solid-svg-icons";
+import { faRoute, faPhone, faLocationDot, faClock, faCheckDouble, faInfoCircle } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Dispatch, SetStateAction, useEffect, useState } from "react";
 import { Data, organizationIcons, organizationType } from "@/app/types/listdata";
-import Kmap from "../kakaomap/Kmap";
 
 type Props = {
     currentData: Data | undefined,
@@ -14,123 +11,135 @@ type OperationData = {
     end: number
 }
 
-export default function PharmBoxInfo(prop: Props) {
-    const [operationInfo, setOperationHour] = useState<OperationData[]>([]); //0~7 월~일
-    const days = ["월", "화", "수", "목", "금", "토", "일", "공휴일"];
+const operationDays = ["월", "화", "수", "목", "금", "토", "일", "공휴일"];
 
-    useEffect(() => {
-        async function getOperationData(data: Data) {
-            const state = data.location.split(" ")[0];
-            const city = data.location.split(" ")[1];
-            const operationData =
-                await fetch(`/api/service/pharm/info?state=${state}&city=${city}&name=${data.name}`);
-            const operationJson = await operationData.json();
-            const operationArray: OperationData[] = [];
+export default async function PharmBoxInfo(prop: Props) {
+    const data = prop.currentData;
+    const operationArray: OperationData[] = [];
 
-            if (operationData.ok) {
-                for (let i = 1; i < 9; i++) {
-                    if(operationJson.data[`dutyTime${i}s`]) {
-                        operationArray.push({
-                            start: operationJson.data[`dutyTime${i}s`],
-                            end: operationJson.data[`dutyTime${i}c`]
-                        });    
-                    }
+    if (data && data.type === "pharm") {
+        const state = data.location.split(" ")[0];
+        const city = data.location.split(" ")[1];
+        const operationData =
+            await fetch(`${process.env.SERVICE_URL}/api/service/pharm/info?state=${state}&city=${city}&name=${data.name}`);
+        const operationJson = await operationData.json();
+
+        if (operationData.ok) {
+            for (let i = 1; i < 9; i++) {
+                if(operationJson.data[`dutyTime${i}s`]) {
+                    operationArray.push({
+                        start: operationJson.data[`dutyTime${i}s`],
+                        end: operationJson.data[`dutyTime${i}c`]
+                    });    
                 }
-                setOperationHour(operationArray);
             }
         }
+    }
 
-        if (prop.currentData && prop.currentData.type === "pharm") {
-            getOperationData(prop.currentData);
-        }
-    }, [prop.currentData]);
-
-    return (
-        prop.currentData && (
-        <>
-            <h2 className="text-3xl font-semibold flex">
-                <span className="w-max text-xl rounded-xl dark:bg-slate-800 bg-slate-200 py-1 px-2 me-2">
-                    <FontAwesomeIcon 
-                    icon={organizationIcons[prop.currentData?.type]}
-                    className="pe-1" />
-                    {organizationType[prop.currentData?.type]}
-                </span>
-                <span className="md:w-auto lg:w-max w-max md:break-all lg:break-keep">{prop.currentData?.name}</span>
-            </h2>
-            <p className="flex gap-2 w-max">
-                <a 
-                    href={`https://map.kakao.com/link/to/${prop.currentData?.name},${prop.currentData?.lat},${prop.currentData?.lng}`}
-                    target="_blank"
-                    className="no-underline p-2 mt-1 hover:bg-slate-200 hover:dark:bg-slate-600 rounded-xl"
-                >
-                    <FontAwesomeIcon icon={faRoute} className="pe-1" />
-                    <span className="font-semibold">길찾기</span>
-                </a>
-                <a
-                    href={`tel:${prop.currentData?.tel}`}
-                    className="no-underline p-2 mt-1 hover:bg-slate-200 hover:dark:bg-slate-600 rounded-xl"
-                >
-                    <FontAwesomeIcon icon={faPhone} className="pe-1" />
-                    <span className="font-semibold">전화하기</span> {prop.currentData?.tel}
-                </a>
-            </p>
-            <p id="location">
-                <FontAwesomeIcon icon={faLocationDot} className="pe-2" />
-                <span>{prop.currentData?.location}</span>
-            </p>
-            {operationInfo.length > 0 && (
-                <section id="operationHours" className="flex mt-2">
-                    <FontAwesomeIcon icon={faClock} className="pe-2 mt-1" />
+    if (data) {
+        return (
+            <div>
+                {/* 수거함 종류 및 이름 */}
+                <h2 className="text-3xl font-semibold flex">
+                    <span className="w-max text-xl rounded-xl dark:bg-slate-800 bg-slate-200 py-1 px-2 me-2">
+                        <FontAwesomeIcon 
+                        icon={organizationIcons[data.type]}
+                        className="pe-1" />
+                        {organizationType[data.type]}
+                    </span>
+                    <span className="md:w-auto lg:w-max w-max md:break-all lg:break-keep">
+                        {data.name}
+                    </span>
+                </h2>
+    
+                {/* 수거함 위치 및 연락처로 연결되는 버튼 모음 */}
+                <ul className="flex gap-2 w-max">
+                    <li>
+                        <a 
+                            href={`https://map.kakao.com/link/to/${data.name},${data.lat},${data.lng}`}
+                            target="_blank"
+                            className="no-underline p-2 mt-1 hover:bg-slate-200 hover:dark:bg-slate-600 rounded-xl"
+                        >
+                            <FontAwesomeIcon icon={faRoute} className="pe-1" />
+                            <span className="font-semibold">길찾기</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            href={`tel:${data.tel}`}
+                            className="no-underline p-2 mt-1 hover:bg-slate-200 hover:dark:bg-slate-600 rounded-xl"
+                        >
+                            <FontAwesomeIcon icon={faPhone} className="pe-1" />
+                            <span className="font-semibold">전화하기</span> {data.tel}
+                        </a>
+                    </li>
+                </ul>
+                
+                {/* 수거함 주소 */}
+                <p id="location">
+                    <FontAwesomeIcon icon={faLocationDot} className="pe-2" />
+                    <span>{data.location}</span>
+                </p>
+    
+                {/* 수거함 소재지의 운영 시간 */}
+                {operationArray.length > 0 && (
+                    <section id="operationHours" className="flex mt-2">
+                        <FontAwesomeIcon icon={faClock} className="pe-2 mt-1" />
+                        <ul>
+                            {operationArray.map((data: OperationData, index) => (
+                                <li key={index}>
+                                    <span className="font-semibold pe-2">{operationDays[index]}</span>
+                                    {data.start >= 1000 ? 
+                                        `${data.start.toString().slice(0, 2)}:${data.start.toString().slice(2)}`
+                                        : `${data.start.toString().slice(0, 1)}:${data.start.toString().slice(1)}`}
+                                    &nbsp;~&nbsp;
+                                    {data.end >= 1000 ? 
+                                        `${data.end.toString().slice(0, 2)}:${data.end.toString().slice(2)}`
+                                        : `${data.end.toString().slice(0, 1)}:${data.end.toString().slice(1)}`}
+                                </li>
+                            ))}
+                        </ul>
+                    </section>
+                )}
+    
+                {/* 수거함 정보 출처 */}
+                <section id="dataSource" className="flex gap-2 mt-2">
+                    <div className="flex items-start">
+                        <FontAwesomeIcon icon={faCheckDouble} className="me-2 mt-1" />
+                        <span className="font-semibold">출처</span>
+                    </div>
                     <ul>
-                        {operationInfo.map((data: OperationData, index) => (
-                            <li key={index}>
-                                <span className="font-semibold pe-2">{days[index]}</span>
-                                {data.start >= 1000 ? 
-                                    `${data.start.toString().slice(0, 2)}:${data.start.toString().slice(2)}`
-                                    : `${data.start.toString().slice(0, 1)}:${data.start.toString().slice(1)}`}
-                                &nbsp;~&nbsp;
-                                {data.end >= 1000 ? 
-                                    `${data.end.toString().slice(0, 2)}:${data.end.toString().slice(2)}`
-                                    : `${data.end.toString().slice(0, 1)}:${data.end.toString().slice(1)}`}
+                        <li>
+                            {`${data.location.split(" ")[0].endsWith("도") ? 
+                                data.location.split(" ")[1]
+                                :data.location.split(" ")[0]} 폐의약품 수거함 공공데이터`}
+                        </li>
+                        {operationArray.length > 0 ? (
+                            <li>
+                                <a href="https://www.nmc.or.kr/nmc/main/contents.do?menuNo=200553" target="blank">
+                                    국립중앙의료원 약국 정보 공공데이터
+                                </a>
                             </li>
-                        ))}
+                        )
+                        : ""}
                     </ul>
                 </section>
-            )}
-            <section id="dataSource" className="flex gap-2 mt-2">
-                <div className="flex items-start">
-                    <FontAwesomeIcon icon={faCheckDouble} className="me-2 mt-1" />
-                    <span className="font-semibold">출처</span>
-                </div>
-                <ul>
-                    <li>
-                        {`${prop.currentData?.location.split(" ")[0].endsWith("도") ? 
-                            prop.currentData?.location.split(" ")[1]
-                            :prop.currentData?.location.split(" ")[0]} 폐의약품 수거함 공공데이터`}
-                    </li>
-                    {operationInfo.length > 0 ? (
-                        <li>
-                            <a href="https://www.nmc.or.kr/nmc/main/contents.do?menuNo=200553" target="blank">
-                                국립중앙의료원 약국 정보 공공데이터
-                            </a>
-                        </li>
-                    )
-                    : ""}
-                </ul>
-            </section>
-            <section id="caution" className="flex gap-2 mt-2">
-                <div className="flex items-start">
-                    <FontAwesomeIcon icon={faInfoCircle} className="me-2 mt-1" />
-                    <span className="font-semibold">안내</span>
-                </div>
-                <p>
-                    공공 데이터 자료는 특성상 기준일 이후 전화번호, 운영 시간 등이 변경될 수 있으므로,&nbsp;
-                    <a href={`https://map.kakao.com/link/search/${prop.currentData?.name}`} target="_blank">카카오맵</a>
-                    &nbsp;혹은&nbsp;
-                    <a href={`https://map.naver.com/p/search/${prop.currentData?.name}`} target="_blank">네이버 지도</a>
-                    에서 장소를 찾아보시거나 직접 전화하시어 다시 한 번 확인하세요.
-                </p>
-            </section>
-        </>
-    ));
+    
+                {/* 수거함 데이터 관련 주의사항 */}
+                <section id="caution" className="flex gap-2 mt-2">
+                    <div className="flex items-start">
+                        <FontAwesomeIcon icon={faInfoCircle} className="me-2 mt-1" />
+                        <span className="font-semibold">안내</span>
+                    </div>
+                    <p>
+                        공공 데이터 자료는 특성상 기준일 이후 전화번호, 운영 시간 등이 변경될 수 있으므로,&nbsp;
+                        <a href={`https://map.kakao.com/link/search/${data.name}`} target="_blank">카카오맵</a>
+                        &nbsp;혹은&nbsp;
+                        <a href={`https://map.naver.com/p/search/${data.name}`} target="_blank">네이버 지도</a>
+                        에서 장소를 찾아보시거나 직접 전화하시어 다시 한 번 확인하세요.
+                    </p>
+                </section>
+            </div>
+        )
+    }
 }
