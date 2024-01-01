62a0aefc4fe66aa4f900d49fd67efaf3d6c68e1f
paperbox <lego37yoon@outlook.com>
2024-01-01 15:08:53 +0900

    (cont) update year mark
    - Happy New Year! 🐉

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
         </div>
         <footer className="mx-4 md:mx-8 my-2 md:my-4 h-[2rem]">
           <p>
-            © 2023 Re:Pharm 프로젝트 및 &nbsp;
+            © 2023-24 Re:Pharm 프로젝트 및 &nbsp;
             <a href="https://github.com/re-pharm/pharm-map/graphs/contributors" target="_blank">기여자</a>. &nbsp;
             <a href="https://github.com/re-pharm/pharm-map/blob/main/README.md" target="_blank">오픈소스 라이선스 확인하기</a>
           </p>
