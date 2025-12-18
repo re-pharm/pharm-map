af17ea84421f6471d9a048dd7ff42bfa39fe510f
paperbox <7126454+lego37yoon@users.noreply.github.com>
2025-12-19 01:41:12 +0900

    (init) set default indent to 2 with vscode integration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,13 +2,15 @@ import { defineConfig } from "eslint/config";
 import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
+import stylistic from "@stylistic/eslint-plugin";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig([{
   extends: [...nextCoreWebVitals],
+  plugins: { "@stylistic": stylistic },
   rules: {
-    "indent": ["error", 2]
+    "@stylistic/indent": ["warn", 2]
   }
 }]);
